c43120ace4e097b182eddb370f08288f4da01e73
Sakib Fakir <FakirSakib22232@gmail.com>
2025-03-13 00:29:59 +0600

    io added on client

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
+import {io} from 'socket.io-client'
+
+
+
+const socket = io('http://localhost:5000/');
 
 
 
 function App() {
+  console.log(socket);
+
+  
 
 
   return (
     <div>
       <h1>Video calling app </h1>
+
+      <section>
+
+      </section>
 
       
 
