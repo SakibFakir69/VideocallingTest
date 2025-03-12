ff378d5e11a54fcefcadc008407e1d8ad5f92817
Sakib Fakir <FakirSakib22232@gmail.com>
2025-03-13 00:43:36 +0600

    connection done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {io} from 'socket.io-client'
-
+// import to client socket.io
 
 
 const socket = io('http://localhost:5000/');
-
+// here use socket io (server link)
 
 
 function App() {
   console.log(socket);
 
-  
+
+  useEffect(()=>{
+   socket.connect();
+
+
+
+
+    return ()=>{
+
+      socket.disconnect();
+
+
+    }
+
+
+
+
+
+  })
+
+
+
+
+
 
 
   return (
