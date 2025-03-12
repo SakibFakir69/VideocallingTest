1533296fa7aa9f8975d8a48f42fc5342f8185d06
Sakib Fakir <FakirSakib22232@gmail.com>
2025-03-13 01:10:40 +0600

    succesfully msg send and welcome msg recive

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,53 @@
-import { useEffect, useState } from 'react'
-import {io} from 'socket.io-client'
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 // import to client socket.io
 
-
-const socket = io('http://localhost:5000/');
+const socket = io("http://localhost:5000/");
 // here use socket io (server link)
 
-
 function App() {
+
+  const [ text , settext ] = useState("");
+
+
+
+
   console.log(socket);
 
+  useEffect(() => {
+    socket.connect();
+    //  when effect run socket connect
 
-  useEffect(()=>{
-   socket.connect();
+    // show welcome message 
+    socket.on("welcome",(msg)=>{
+      console.log("welcome msg",msg);
+
+    })
 
 
 
-
-    return ()=>{
-
+    return () => {
       socket.disconnect();
+      // remove connect
+    };
+
+  },[]);
 
 
-    }
+  // sendMessage
+  
+  const sendMessage = (event)=>{
+    event.preventDefault();
+    // here fetch text 
+    // chat
+    socket.emit('chat',text);
 
 
 
 
 
-  })
 
-
-
+  }
 
 
 
@@ -41,13 +57,17 @@ function App() {
       <h1>Video calling app </h1>
 
       <section>
+        {/* make a send message option  */}
+        <form onSubmit={sendMessage}>
+          <input type="text" value={text} onChange={(e)=> settext(e.target.value)} placeholder="Enter your text" />
+
+          <button type="submit">Send</button>
+        </form>
+
 
       </section>
-
-      
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
