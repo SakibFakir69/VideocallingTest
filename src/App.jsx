6b0d5379895aa8b1851e2eb88cd6c35bd82706c7
Sakib Fakir <FakirSakib22232@gmail.com>
2025-03-14 02:56:32 +0600

    user to user tes t

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,71 @@
-import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
-// import to client socket.io
 
-const socket = io("http://localhost:5000/");
-// here use socket io (server link)
+
+
+
+
+
+import React, { useEffect, useState } from 'react'
+
+import {io} from 'socket.io-client'
+
+const socket = io('http://localhost:5000')
 
 function App() {
-
-  const [ text , settext ] = useState("");
-  const [ messges , setmessages ] = useState([]);
-  /// store message , get this message from board cast user 
+  console.log("socket ", socket);
 
 
+  // recive msg use a 
+  const [ message , setmessage ] = useState('');
 
+  // socket conection 
 
+  useEffect(()=>{
 
-  console.log(socket);
-
-  useEffect(() => {
     socket.connect();
-    //  when effect run socket connect
-
-    // show welcome message 
-    socket.on("welcome",(msg)=>{
-      console.log("welcome msg",msg);
-
-    })
 
 
 
-    return () => {
+
+
+
+    return ()=>{
       socket.disconnect();
-      // remove connect
-    };
+    }
 
-  },[]);
-  console.log(messges);
+  })
 
+  // send message fucntion
 
-  // sendMessage
-  
   const sendMessage = (event)=>{
     event.preventDefault();
-    // here fetch text 
-    // chat
-    socket.emit('chat',text);
-    settext(" ");
+    // send msg to server 
+    /// client use emit to send data 
 
-
-
-
+    socket.emit('send-message',message)
 
 
   }
-
-      //  this is borad cast message get everyone 
-      socket.on('chat',(msg)=>{
-        console.log("broad cast ",msg);
-        setmessages((prev)=> [...prev,msg]);
-    
-      })
-    
   
-
-
 
 
 
   return (
     <div>
-      <h1>Video calling app </h1>
+      <h2>Next call</h2>
 
-      <section>
-        {/* make a send message option  */}
-        <form onSubmit={sendMessage}>
-          <input type="text" value={text} onChange={(e)=> settext(e.target.value)} placeholder="Enter your text" />
+      <section className='flex justify-center mt-10'>
+       
+       <form onClick={sendMessage}>
+       <input value={message} onChange={(e)=> setmessage(e.target.value)} type="text" placeholder='Enter your message ...'  className='border py-1' />
 
-          <button type="submit">Send</button>
-        </form>
-
-
+        <button className='border px-8 py-1 bg-red-500 text-white rounded cursor-pointer'>Send</button>
+       </form>
       </section>
 
-      {/* this section show message */}
-
-      <section>
-        {
-          messges.map((msg,key)=>(
-            <li>{msg}</li>
-          ))
-        }
-      </section>
 
 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
