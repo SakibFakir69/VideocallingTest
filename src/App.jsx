fc5f0f8dcbbeb878714271a768bd19e80088f1a0
Sakib Fakir <FakirSakib22232@gmail.com>
2025-03-15 15:51:12 +0600

    client sockt io use room chat

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,41 @@
 
 
 
-
 import React, { useEffect, useState } from 'react'
-
 import {io} from 'socket.io-client'
 
 const socket = io('http://localhost:5000')
 
 function App() {
-  console.log("socket ", socket);
 
+  const [ room , setroom ] = useState('');
+  const [ welcomemsg , setwelcomemsg ] = useState('');
+  const [ message , setmessage ] = useState("");
 
-  // recive msg use a 
-  const [ message , setmessage ] = useState('');
-
-  // socket conection 
+  const [ chat , setchat ] = useState([]);
+  const [ socketid , setsocketid ] = useState("")
 
   useEffect(()=>{
 
     socket.connect();
+    socket.on('connect',()=>{
+      console.log(socket.id);
+      setsocketid(socket.id);
+    })
+
+    // welcomne msg
 
 
+    socket.on('welcome',(msg)=>{
+      setwelcomemsg(msg);
+
+    })
+    // servermessage
+
+    socket.on('servermessage',(msg,id)=>{
+      console.log(msg ," df",id);
+      setchat((prev)=> [...prev,msg]);
+    })
 
 
 
@@ -32,36 +46,79 @@ function App() {
       socket.disconnect();
     }
 
-  })
 
-  // send message fucntion
+  },[])
 
-  const sendMessage = (event)=>{
+  // 
+  const handelSubmit = (event)=>{
     event.preventDefault();
-    // send msg to server 
-    /// client use emit to send data 
-
-    socket.emit('send-message',message)
+    console.log(room);
+    socket.emit('join-room',room);
 
 
   }
-  
+  const sendMessage= (event)=>{
+    event.preventDefault();
+
+    socket.emit('message',{msg:message,id:socketid});
+
+
+  }
+
+
+
+
 
 
 
   return (
-    <div>
-      <h2>Next call</h2>
+    <div className='w-full min-h-screen'>
 
-      <section className='flex justify-center mt-10'>
-       
-       <form onClick={sendMessage}>
-       <input value={message} onChange={(e)=> setmessage(e.target.value)} type="text" placeholder='Enter your message ...'  className='border py-1' />
+      <section className='flex justify-center items-center  border h-screen'>
+    
+        <form onSubmit={handelSubmit} className=''>
 
-        <button className='border px-8 py-1 bg-red-500 text-white rounded cursor-pointer'>Send</button>
-       </form>
+          <input type='text' placeholder='Enter your room name ' className='py-3 border px-10 rounded' value={room} onChange={(e)=> setroom(e.target.value)}/>
+          <button type='submit' className='px-8 py-2.5 bg-green-500 text-white font-semibold'>Submit</button>
+
+        </form>
+
+
       </section>
+      {
+        welcomemsg && (<div>
+          <h1>You join this room {welcomemsg}</h1>
 
+          {/* send message  */}
+
+          <section>
+
+
+            <form onSubmit={sendMessage}>
+
+              <input type='text' placeholder='Enter your message' value={message} onChange={(e)=> setmessage(e.target.value)}/>
+
+              <button type='submit'>Send</button>
+            </form>
+          </section>
+
+          <section>
+           
+           {
+            chat.map((msg,key)=>(
+              
+              <div key={key}>
+                <p className='bg-red-600'>{msg}</p>
+
+              </div>
+            ))
+           }
+          </section>
+
+        </div>
+        )
+      }
+    
 
 
     </div>
